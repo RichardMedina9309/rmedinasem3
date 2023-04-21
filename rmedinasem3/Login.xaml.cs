4459023bbc9211f5a5cecde228b1c5d95ec82b69
richa <richa@DESKTOP-689S21F>
2023-04-21 18:55:02 -0500

    Agregar archivos de proyecto.

---
+++ rmedinasem3/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rmedinasem3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contraseña = txtContraseña.Text;
+            if (usuario =="rmedina" && contraseña == "12345")
+            {
+                Navigation.PushAsync(new Principal());
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario/Contraseña incorrectos", "Cerrar");
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+            }
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
